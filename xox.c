4395c0d7eb5b6cab89ab4d374fd1b430cbd5edce
sabithk <75925743+sabithk@users.noreply.github.com>
2021-10-31 22:53:27 +0530

    Update xox.c

--- xox.c
+++ xox.c
@@ -4,7 +4,7 @@ char a[9]={'1','2','3','4','5','6','7','8','9'};
 int f=0;
 char re;
 void draw(){
-	printf("\n\n\t\t\t X O X  GAME ");
+	printf("\n\n\t\t\t Tick Tack Toe GAME ");
 	printf("\n\n\t\t\t|---|---|---|\n");
 		printf("\t\t\t|  %c|  %c| %c |\n",a[0],a[1],a[2]);
 	printf("\t\t\t|---|---|---|\n");
